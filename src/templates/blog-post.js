41b4cf50b4febff3584e7a9489d52ef6a95bb838
siantarbunga <82822326+siantarbunga@users.noreply.github.com>
2021-04-21 12:30:26 +0700

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import html from "../components/html"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
