ee924ccda35c7fc0ee030cd4e4cebee8fabfb17a
siantarbunga <82822326+siantarbunga@users.noreply.github.com>
2021-04-21 12:33:56 +0700

    Delete html.js

--- src/components/html.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZDFTR9VGTH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZDFTR9VGTH');
-</script>
